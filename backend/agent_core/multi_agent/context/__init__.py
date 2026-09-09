bdc78e8b6635bc44a712ce6acdae6de927bafd1b
mivaille777 <2735545778@qq.com>
2026-09-09 11:53:44 +0800

    chore(stage5.6): export knowledge and memory context adapters

--- backend/agent_core/multi_agent/context/__init__.py
+++ backend/agent_core/multi_agent/context/__init__.py
@@ -1,4 +1,12 @@
-from .shared_context import SharedAgentContext
 from .context_manager import SharedContextManager
+from .knowledge_injector import KnowledgeInjector
+from .memory_adapter import AgentMemoryAdapter, InMemoryAgentMemoryStore
+from .shared_context import SharedAgentContext
 
-__all__ = ["SharedAgentContext", "SharedContextManager"]
+__all__ = [
+    "SharedAgentContext",
+    "SharedContextManager",
+    "KnowledgeInjector",
+    "AgentMemoryAdapter",
+    "InMemoryAgentMemoryStore",
+]
